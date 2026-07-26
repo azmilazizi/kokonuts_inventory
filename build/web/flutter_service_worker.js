a6120cbe703578b4b3079ab02588f37320194c79
azmilazizi <azmilazizi1999@graduate.utm.my>
2026-07-26 15:53:15 +0800

    loss adjutment check

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1de4bbfcfe8f45885da9476ee418ba62",
+const RESOURCES = {"flutter_bootstrap.js": "7332ee38392e05165dbada3afb6163a8",
 "version.json": "238747f89919f9d40a20959bc47888fc",
 "index.html": "ce4760fa99bb39d4d37f5c9d9502b666",
 "/": "ce4760fa99bb39d4d37f5c9d9502b666",
-"main.dart.js": "2a2c089927dac083c997a7c02d1c5f1b",
+"main.dart.js": "d99c9e5d8731a22de5a034497b53ab35",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "68914539c67a04b9556a8cf516bb6979",
 "icons/app_logo_light.png": "1a92dccce2f476e1f1b3ed9f49553178",
